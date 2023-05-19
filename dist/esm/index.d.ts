da0d740e49f6efe96f683cf9740b163faaca3534
Ayax0 <simon.gander@vistectic.com>
2023-05-19 11:45:24 +0200

    change import style

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -1,14 +1,1 @@
-export interface SchemaOptions {
-    reduce?: boolean;
-    strictType?: boolean;
-}
-export default class ObjectSchema<T> {
-    private schema;
-    private readonly DEFAULT_OPTIONS;
-    constructor(schema: Object);
-    filter(item: any | Array<any>, options?: SchemaOptions): Promise<T>;
-    private filterObject;
-    private filterArray;
-    private formatField;
-    private generateEmptyObject;
-}
+export * from "./ObjectSchema";
